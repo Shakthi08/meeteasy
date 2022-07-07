d90faaeab569f382faf71d13d7f7d3b38aa6fb9d
Shakthi <nisha.brindavan@gmail.com>
2022-07-07 12:51:58 +0100

    continous login in state

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:meeteasy/screens/home_screen.dart';
 import 'package:meeteasy/screens/login_screen.dart';
 import 'package:meeteasy/utils/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:meeteasy/resources/auth_methods.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,15 +18,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Meet now',
-      theme:
-          ThemeData.dark().copyWith(scaffoldBackgroundColor: backgroundColor),
-      routes: {
-        '/login': (context) => const LoginScreen(),
-        '/home': (context) => const HomeScreen(),
-      },
-      home: const LoginScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Meet now',
+        theme:
+            ThemeData.dark().copyWith(scaffoldBackgroundColor: backgroundColor),
+        routes: {
+          '/login': (context) => const LoginScreen(),
+          '/home': (context) => const HomeScreen(),
+        },
+        home: StreamBuilder(
+          stream: AuthMethods().authChanges,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.hasData) {
+              return const HomeScreen();
+            }
+            return const LoginScreen();
+          },
+        ));
   }
 }
